cdffd8fb4c1bf762a584b41075c4aaa2fc9e4878
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2022-11-10 11:38:35 +0530

    MongoDB Connection

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
+
+//Connect Database
+connectDB();
+
+//Init Middleware 
+app.use(express.json({ extended:false }));
 
 app.get('/',(req,res) => res.send('API Running'))
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
