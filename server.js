43d917b04ac05e3c35acf2bbba4a25a6924a58a2
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2022-11-11 23:00:48 +0530

    Create User Model + Register User + Authenticate User + Login User

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ connectDB();
 //Init Middleware 
 app.use(express.json({ extended:false }));
 
-app.get('/',(req,res) => res.send('API Running'))
+app.get('/',(req,res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'))
 
 const PORT = process.env.PORT || 5000;
 
